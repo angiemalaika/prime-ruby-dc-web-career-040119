165f4529c1f50372ef0126ffca821274ac1f7db5
Angela Mumba <malaika.mumba@gmail.com>
2019-03-28 04:34:57 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,10 @@
-def prime?(integer)
-  test_divisors = (2..(integer - 1)).to_a	  test_divisors = (2..(integer - 1)).to_a
-  
-  (test_divisors.any? {|divisor| integer % divisor == 0} || integer == 0 
-   integer == 1) ? false : true
- end 
+def prime?(integer)	
+  test_divisors = (2..(integer - 1)).to_a
+if test_divisors.any? {|divisor| integer % divisor == 0}
+   || integer == 0 || integer == 1
+
+false
+else
+  true
+end
+end 
